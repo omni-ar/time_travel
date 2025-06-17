3c7379c79e59aa531c3280ee42bdd923a21141aa
arjit <arjittripathi3103@gmail.com>
2025-06-17 19:49:27 +0530

    Updating cloud file upload

--- BACKEND/controllers/diaryController.js
+++ BACKEND/controllers/diaryController.js
@@ -28,9 +28,9 @@ const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
